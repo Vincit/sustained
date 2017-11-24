d9281b6e60c5373ffb39631b2e19a89506c8daf5
Sami Koskimäki <sami.koskimaki@vincit.com>
2017-11-24 15:59:33 +0200

    move the experimental compiler to a class

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const { QueryBuilder } = require('./lib/queryBuilder/QueryBuilder');
+const { QueryCompiler } = require('./lib/compilers/QueryCompiler');
+const { QueryBuilder } = require('./lib/builders/QueryBuilder');
 const { AstBuilder } = require('./lib/ast/AstBuilder');
 const { parseRaw } = require('./lib/ast/parsers/parseRaw');
 
 module.exports = {
   QueryBuilder,
+  QueryCompiler,
   AstBuilder,
   raw: parseRaw
 };
--- tests/unit/compilerPlayground.js
+++ tests/unit/compilerPlayground.js
@@ -1,8 +1,8 @@
 const { expect } = require('chai');
-const { QueryBuilder, raw } = require('../../');
+const { QueryBuilder, QueryCompiler, raw } = require('../../');
 const { query, logAst } = require('../testUtils');
 
-describe.skip('compiler playground', () => {
+describe('compiler playground', () => {
   it('test', () => {
     const builder = query()
       .select('a', 'b')
@@ -21,140 +21,8 @@ describe.skip('compiler playground', () => {
           .where('id', 1);
       });
 
-    const compiler = {
-      bindings: [],
+    const { sql, bindings } = QueryCompiler.compile(builder.ast);
 
-      QueryNode(node, { parent }) {
-        const isSubQuery = !!parent;
-        let sql = '';
-
-        if (isSubQuery || node.alias) {
-          sql += '(';
-        }
-
-        sql += 'SELECT ';
-
-        sql += node.select.map((it, idx) => it.visit(this, { parent: node, idx })).join(', ');
-
-        sql += ' FROM ';
-
-        sql += node.from.map((it, idx) => it.visit(this, { parent: node, idx })).join(', ');
-
-        if (node.where.length) {
-          sql += ' WHERE ';
-
-          node.where.forEach((it, idx) => (sql += it.visit(this, { parent: node, idx })));
-        }
-
-        if (isSubQuery || node.alias) {
-          sql += ')';
-        }
-
-        if (node.alias) {
-          sql += ' as ' + node.alias.visit(this, { parent: node });
-        }
-
-        return sql;
-      },
-
-      WhereNode(node, { parent, idx }) {
-        let sql = '';
-
-        if (idx !== 0) {
-          sql += node.bool + ' ';
-        }
-
-        if (idx !== 0 && node.not) {
-          sql += 'not ';
-        }
-
-        if (node.lhs) {
-          sql += node.lhs.visit(this, { parent: node }) + ' ';
-        }
-
-        if (node.op) {
-          sql += node.op.visit(this, { parent: node }) + ' ';
-        }
-
-        if (node.rhs) {
-          sql += node.rhs.visit(this, { parent: node }) + ' ';
-        }
-
-        return sql;
-      },
-
-      SelectNode(node, { parent }) {
-        let sql = '';
-
-        sql += node.selection.visit(this, { parent: node });
-
-        if (node.alias) {
-          sql += ' as ' + node.alias.visit(this, { parent: node });
-        }
-
-        return sql;
-      },
-
-      FromNode(node, { parent }) {
-        let sql = '';
-
-        sql += node.from.visit(this, { parent: node });
-
-        if (node.alias) {
-          sql += ' as ' + node.alias.visit(this, { parent: node });
-        }
-
-        return sql;
-      },
-
-      ValueNode(node) {
-        return this.nextBinding(node.value);
-      },
-
-      OperatorNode(node) {
-        return node.operator;
-      },
-
-      IdentifierNode(node) {
-        return node.ids.map(compiler.wrapIdentifier).join('.');
-      },
-
-      RawNode(node) {
-        const origSql = node.sql;
-
-        let sql = '';
-        let prevEnd = 0;
-
-        node.bindings.forEach(binding => {
-          const { match, index, node: bindNode } = binding;
-
-          sql += origSql.slice(prevEnd, index);
-          sql += bindNode.visit(this, { parent: node });
-
-          prevEnd = index + match.length;
-        });
-
-        sql += origSql.slice(prevEnd, origSql.length);
-
-        return sql;
-      },
-
-      wrapIdentifier(id) {
-        if (id === '*') {
-          return id;
-        } else {
-          return '"' + id + '"';
-        }
-      },
-
-      nextBinding(value) {
-        this.bindings.push(value);
-        return '$' + this.bindings.length;
-      }
-    };
-
-    const sql = builder.ast.visit(compiler, {});
-
-    console.log(sql, compiler.bindings);
+    console.log(sql, bindings);
   });
 });
