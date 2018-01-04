19eb0d3fd723655e6f0c19794e1c8244435e511a
Sami Koskimäki <sami.koskimaki@vincit.com>
2018-01-04 08:45:03 +0200

    first draft of the postgres dialect  * We have running queries, yay!

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+const { PoolingConnectionSource } = require('./lib/driver/PoolingConnectionSource');
+const { SingleConnectionSource } = require('./lib/driver/SingleConnectionSource');
 const { QueryCompiler } = require('./lib/compiler/QueryCompiler');
 const { QueryBuilder } = require('./lib/builder/QueryBuilder');
 const { raw } = require('./lib/builder/RawBuilder');
 
 module.exports = {
+  PoolingConnectionSource,
+  SingleConnectionSource,
   QueryBuilder,
   QueryCompiler,
   raw
---
+++ tests/integration/index.js
@@ -0,0 +1,58 @@
+const { expect } = require('chai');
+const { QueryBuilder } = require('../../');
+const { PoolingConnectionSource } = require('../../');
+
+const pg = require('pg');
+const {
+  Compiler: PostgreSQLQueryCompiler,
+  Driver: PostgreSQLDriver
+} = require('../../lib/dialect/postgresql');
+
+describe('integration tests', () => {
+  const compiler = new PostgreSQLQueryCompiler();
+
+  const driver = new PostgreSQLDriver({
+    pg,
+    connectionConfig: {
+      database: 'objection_test'
+    }
+  });
+
+  const connectionSource = new PoolingConnectionSource({ driver, poolConfig: { min: 0, max: 10 } });
+
+  it('my first real query', () => {
+    const query = new QueryBuilder();
+
+    return query
+      .from('Model1')
+      .select('*')
+      .where('model1Prop1', 'hello 5')
+      .execute({
+        compiler,
+        connectionSource
+      })
+      .then(rows => {
+        console.log(rows);
+      });
+  });
+
+  it('my second real query', () => {
+    const query = new QueryBuilder({
+      compiler,
+      connectionSource
+    });
+
+    return query
+      .from('Model1')
+      .select('*')
+      .whereIn('id', [1, 2, 3])
+      .orderBy('id')
+      .then(rows => {
+        console.log(rows);
+      });
+  });
+
+  after(() => {
+    return connectionSource.destroy();
+  });
+});
